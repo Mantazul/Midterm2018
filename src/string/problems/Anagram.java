77dbc373416b87750fab5834b41a57e1cda6d1e4
afia <afiafarjana560@gmail.com>
2018-09-09 18:54:38 -0400

    updated

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -6,17 +6,13 @@ import java.util.Arrays;
  * Created by mrahman on 04/22/17.
  */
 public class Anagram {
-
-    public static void main(String[] args) {
-        //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
-        //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
-
-        String s1 = "ARMY";
-        String s2 = "MARY";
+    public static void isAnagram(String str1, String str2){
+        String s1 = str1;
+        String s2 = str2;
         int[] s1Ascii = new int[s1.length()];
         int[] s2Ascii = new int[s2.length()];
 
-        if(s1.length()!=s2.length()) System.out.println("Not anagram.");
+        if(s1.length()!=s2.length()) System.out.println(s1+" "+s2+" are not anagram.");
         else {
             for(int i=0; i<s1.length();i++){
                 s1Ascii[i]= (int) s1.charAt(i);
@@ -26,9 +22,19 @@ public class Anagram {
             Arrays.sort(s1Ascii);
             Arrays.sort(s2Ascii);
 
-            if(Arrays.equals(s1Ascii,s2Ascii)) System.out.println("Anagram");
-            else System.out.println("Not anagram");
+            if(Arrays.equals(s1Ascii,s2Ascii)) System.out.println(s1+" "+s2+" are Anagram");
+            else System.out.println(s1+" "+s2+" are not anagram");
 
         }
+    }
+
+    public static void main(String[] args) {
+        //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
+        //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+        isAnagram("ARMY","MARY");
+        isAnagram("CAT","ACT");
+        isAnagram("NOT","TOP");
+
     }
 }
--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -1,26 +1,35 @@
 package string.problems;
 
 public class Palindrome {
+
+    public static void isPalindrome(String string) {
+   String word = string;
+    StringBuilder palindrom = new StringBuilder();
+
+    int wordLength = word.length();
+
+        for(int i = 0; i<wordLength;i++) {
+        palindrom.append(word.charAt(wordLength - 1 - i));
+    }
+
+    String revPalindrom = new String(palindrom);
+
+        if(word.matches(revPalindrom)) System.out.println(word+" is palindrome.");
+        else System.out.println(word+ " is not palindrome.");
+
+
+
+
+}
     public static void main(String[] args) {
         /*
           If a String is reversed and it remains unchanged, that is called Palindrome. For example, MOM,DAD,MADAM are
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
-        String word = "MOM";
-        StringBuilder palindrom = new StringBuilder();
+        isPalindrome("MOM");
+        isPalindrome("DaD");
+        isPalindrome("MaMa");
 
-        int wordLength = word.length();
-
-        for(int i=0;i<wordLength;i++){
-            palindrom.append(word.charAt(wordLength-1-i));
-        }
-
-        String revPalindrom = new String(palindrom);
-
-        if(word.matches(revPalindrom)){
-            System.out.println("The words are palindrom.");
-        }
-        else System.out.println("The words are not palindrom.");
     }
 }
