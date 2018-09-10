ec88b3a442c96843974bf7d8253293f28d349943
afia <afiafarjana560@gmail.com>
2018-09-10 10:43:22 -0400

    bubbleSort,MergeSort and quickSort added

--- src/algorithm/Sort.java
+++ src/algorithm/Sort.java
@@ -45,39 +45,137 @@ public class Sort {
     }
 
     public int[] bubbleSort(int [] array){
+        final long startTime = System.currentTimeMillis();
         int [] list = array;
-        //implement here
-
-        
-        
+        for(int i=0;i<list.length;i++){
+            for(int j=0;j<list.length-1;j++){
+                if(list[j]>list[j+1]){
+                    int temp = list[j];
+                    list[j] = list[j+1];
+                    list[j+1]=temp;
+                }
+            }
+        }
+        final long endTime = System.currentTimeMillis();
+        final long executionTime = endTime - startTime;
+        this.executionTime = executionTime;
         return list;
     }
-    
 
-    public int [] mergeSort(int [] array){
+    void merge(int list[], int low, int middle, int high)
+    {
+        int i, j, k;
+        int leftArrayLength = middle - low + 1;
+        int rightArrayLength =  high - middle;
+
+        int[] leftArray=new int[leftArrayLength];
+        int[] rightArray = new int[rightArrayLength];
+
+        for (i = 0; i < leftArrayLength; i++)
+            leftArray[i] = list[low + i];
+        for (j = 0; j < rightArrayLength; j++)
+            rightArray[j] = list[middle + 1+ j];
+
+        i = 0;
+        j = 0;
+        k = low;
+        while (i < leftArrayLength && j < rightArrayLength)
+        {
+            if (leftArray[i] <= rightArray[j])
+            {
+                list[k] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                list[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < leftArrayLength)
+        {
+            list[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < rightArrayLength)
+        {
+            list[k] = rightArray[j];
+            j++;
+            k++;
+        }
+    }
+
+
+    public int [] mergeSort(int [] array,int firstIndex,int lastIndex){
+        final long startTime = System.currentTimeMillis();
         int [] list = array;
-        //implement here
-        
-        
+        int low = firstIndex;
+        int high = lastIndex;
+        if (low < high)
+        {
+            int middle = low+(high-low)/2;
+            mergeSort(list, low, middle);
+            mergeSort(list, middle+1, high);
 
+            merge(list, low, middle, high);
+        }
+
+        final long endTime = System.currentTimeMillis();
+        final long executionTime = endTime - startTime;
+        this.executionTime = executionTime;
         return list;
     }
-    
 
-    public int [] quickSort(int [] array){
+    int partition (int list[], int low, int high)
+    {
+        int pivot = list[high];
+        int i = (low - 1);
+
+        for (int j = low; j <= high- 1; j++)
+        {
+            if (list[j] <= pivot)
+            {
+                i++;
+                int temp = list[i];
+                list[i]=list[j];
+                list[j] = temp;
+
+            }
+        }
+
+        int temp = list[i+1];
+        list[i+1]=list[high];
+        list[high] = temp;
+        return (i + 1);
+    }
+
+    public int [] quickSort(int [] array,int firstIndex, int lastIndex){
+        final long startTime = System.currentTimeMillis();
         int [] list = array;
-        //implement here
-        
-        
+        int low = firstIndex;
+        int high = lastIndex;
+        if (low < high)
+        {
+            int pi = partition(list, low, high);
+            quickSort(list, low, pi - 1);
+            quickSort(list, pi + 1, high);
+        }
 
+        final long endTime = System.currentTimeMillis();
+        final long executionTime = endTime - startTime;
+        this.executionTime = executionTime;
         return list;
     }
     
     public int [] heapSort(int [] array){
         int [] list = array;
-        //implement here
-        
-        
+
+
+
 
         return list;
     }
